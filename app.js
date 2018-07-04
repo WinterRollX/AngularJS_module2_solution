6ab1383d3ea91bc0d885c5f2378e41066f863dcf
Winterroll <winterrollxp@gmail.com>
2018-07-03 22:42:49 -0400

    almost finished

--- app.js
+++ app.js
@@ -7,27 +7,64 @@
 	ToBuyController.$inject = ['ShoppingListCheckOffService'];
 	function ToBuyController(ShoppingListCheckOffService) {
 		var toBuyList = this;
-		toBuyList.itemName = ShoppingListCheckOffService.itemName;
+		toBuyList.itemList = ShoppingListCheckOffService.getToBuyList();
+		toBuyList.addToBrought = function (itemIndex) {
+			var tempItem = toBuyList.itemList[itemIndex];
+			ShoppingListCheckOffService.addToBrought(tempItem);
+			ShoppingListCheckOffService.deleteFromToBuy(itemIndex);
+		}
 		
 	}
 
 
-
-
-
 	AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 	function AlreadyBoughtController(ShoppingListCheckOffService) {
-		var	boughtList = this;
-
+		var	bbb = this;
+		bbb.itemList = ShoppingListCheckOffService.getBroughtList();
 	}
 
 
 	
 	function ShoppingListCheckOffService() {
 		var service = this;
+		var toBuyItems = [];
+		var broughtItems = [];
 		
 
+		service.addToToBuy = function (itemName,itemQuantity) {
+			var tempItem = new Item(itemName,itemQuantity);
+			toBuyItems.push(tempItem);
+		}
 
+		service.getToBuyList = function(){
+			return toBuyItems;
+
+		}
+		service.getBroughtList = function () {
+			console.log(broughtItems.length);
+			return broughtItems;
+		}
+
+		service.addToBrought = function (item) {
+			broughtItems.push(item);
+		}
+
+		service.deleteFromToBuy = function (itemIndex) {
+			toBuyItems.splice(itemIndex,1);
+		}
+
+		// push items into toBuy array
+		for(var i=0;i<7;i++){
+		 	var tempItemName = "IamItem" + i;
+		 	service.addToToBuy(tempItemName,i);
+		 }
+		 console.log(toBuyItems);
+
+	}
+
+	function Item(itemName,itemQuantity) {
+		this.itemName = itemName;
+		this.itemQuantity = itemQuantity;
 	}
 
 
